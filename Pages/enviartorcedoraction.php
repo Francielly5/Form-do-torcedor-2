1df527960b961c5493692ec8a4ccc3692599936d
Francielly <franciellym.ferreira15@gmail.com>
2025-11-03 22:25:40 -0300

    Corrigindo 03.11

--- Pages/enviartorcedoraction.php
+++ Pages/enviartorcedoraction.php
@@ -5,11 +5,11 @@ echo"=====================================";
 //echo"<p> Torcedor: {$_POST['nome']} </p>";//
 echo" <p> CPF: " . $_POST ['cpf'] . "</p>";
 echo"<p> Frequencia" . $_POST['frequencia'] . "</p>";
-$socioTorcedor = isset($_POST['socio']) ? $_POST['socio'] : "Sim" ;
-echo "<p> É socio torcedor?" . $_POST['socio'] . "</p>";
+$socioTorcedor = isset($_POST['socioTorcedor']) ? $_POST['socioTorcedor'] : "Não" ;
+echo "<p> É socio torcedor?" . $_POST['socioTorcedor'] . "</p>";
  
-$camisaoficial = isset($_POST['camiseta']) ? $_POST['camiseta'] : "Sim" ;
-echo "<p> Possui camisa oficial?" . $_POST['camiseta'] . "</p>";
+$camisaoficial = isset($_POST['camisaoficial']) ? $_POST['camisaoficial'] : "Não" ;
+echo "<p> Possui camisa oficial?" . $_POST['camisaoficial'] . "</p>";
  
 echo "<p> Qual o time de coração?" . $_POST['time']  . "<?p>";
  
