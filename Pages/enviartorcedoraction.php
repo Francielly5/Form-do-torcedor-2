ffd754362c3e4259fec88a8b87651f08be70faa2
Francielly <franciellym.ferreira15@gmail.com>
2025-11-03 22:13:42 -0300

    03.11 $_POST

---
+++ Pages/enviartorcedoraction.php
@@ -0,0 +1,20 @@
+<?php
+echo "<h1> Dados enviados </h1>";
+echo"<p>Torcedor " . $_POST['nome']  . "</p>";
+echo"=====================================";
+//echo"<p> Torcedor: {$_POST['nome']} </p>";//
+echo" <p> CPF: " . $_POST ['cpf'] . "</p>";
+echo"<p> Frequencia" . $_POST['frequencia'] . "</p>";
+$socioTorcedor = isset($_POST['socio']) ? $_POST['socio'] : "Sim" ;
+echo "<p> É socio torcedor?" . $_POST['socio'] . "</p>";
+ 
+$camisaoficial = isset($_POST['camiseta']) ? $_POST['camiseta'] : "Sim" ;
+echo "<p> Possui camisa oficial?" . $_POST['camiseta'] . "</p>";
+ 
+echo "<p> Qual o time de coração?" . $_POST['time']  . "<?p>";
+ 
+var_dump($_POST);
+var_dump($_GET);
+ 
+ 
+?>
